fca11fad49b6734b9a5adb16cd76c3e4f5d812c6
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2026-05-03 12:55:25 -0500

    chore: update test data

--- tests/generate_data.py
+++ tests/generate_data.py
@@ -4,7 +4,7 @@ def create_mock_data():
     data = {
         "user_id": [1, 2, 3, 4, 5],
         "email": ["alice@example.com", "bob@example.com", "charlie@example.com", "elle@example.com", "eve@example.com"],
-        "age": [25, 30, 15, 40, 22],
+        "age": [25, 30, 15, -40, 22],
         "birth_date": ["2000-10-09", "2003-12-11", "2000-10-20", "2000-03-03", "2000-10-23"]
     }
 
