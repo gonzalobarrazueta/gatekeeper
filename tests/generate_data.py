559b71ae0dfe6ccb13e9031d09c828dd0f4e464b
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2026-05-03 11:54:36 -0500

    chore: add `birth_date` column and update row values

--- tests/generate_data.py
+++ tests/generate_data.py
@@ -3,8 +3,9 @@ import pandas as pd
 def create_mock_data():
     data = {
         "user_id": [1, 2, 3, 4, 5],
-        "email": ["alice@example.com", "bob@example.com", "charlie@example.com", None, "eve@example.com"],
-        "age": [25, 30, -5, 40, 22]
+        "email": ["alice@example.com", "bob@example.com", "charlie@example.com", "elle@example.com", "eve@example.com"],
+        "age": [25, 30, 15, 40, 22],
+        "birth_date": ["2000-10-09", "2003-12-11", "2000-10-20", "2000-03-03", "2000-10-23"]
     }
 
     df = pd.DataFrame(data)
